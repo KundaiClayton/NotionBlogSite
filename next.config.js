ae30f17b85af82ae43af7f89e7a603a9ce32cafb
Kundai <kundai@mohara.co>
2023-02-20 21:11:48 +0200

    [ADDED] turned app into pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// };
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+
+// const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+});
+module.exports = withPWA({
+  reactStrictMode: true,
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+  output: "standalone",
+});
